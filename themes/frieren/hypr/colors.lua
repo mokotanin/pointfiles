e01aafd8fbc326347ee64f3df7002ccead908816
mokotanin <titchou68@gmail.com>
2026-09-20 14:24:09 +0200

    new themes

---
+++ themes/frieren/hypr/colors.lua
@@ -0,0 +1,10 @@
+local palette = require("catppuccin").mocha()
+
+return {
+    accent1 = palette.green.hex,
+    accent2 = palette.red.hex,
+    accent3 = palette.teal.hex,
+    accent4 = palette.yellow.hex,
+    inactive = palette.blue.hex,
+    shadow = palette.mantle.hex,
+}
---
+++ themes/supaS/hypr/colors.lua
@@ -0,0 +1,8 @@
+local palette = require("catppuccin").latte()
+
+return {
+    accent1 = palette.crust.hex,
+    accent2 = palette.mantle.hex,
+    inactive = palette.surface0.hex,
+    shadow = "#191919",
+}
